9f6647b810fb6a9e966f2d465b714d3bc5505ab8
kaiawada <kaiawada.2007h7@gmail.com>
2026-06-09 04:50:59 +0300

    Next.js 15+のparams非同期仕様に対応

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -11,7 +11,6 @@ export default async function BlogTop() {
             <h2>この画面はブログのトップページです</h2>
             <ul>
                 <li><Link href = "/">Home</Link></li>
-                <li><Link href="/blogs/sorevi">Sorevi</Link></li>
                 <li><Link href="/blogs/note">Note</Link></li>
             </ul>
             {blogs.map((article)=> (
