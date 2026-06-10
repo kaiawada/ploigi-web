ab89cc3b3e41726d77b0033544cdc118301de774
kaiawada <kaiawada.2007h7@gmail.com>
2026-06-10 22:48:16 +0300

    トップページデザイン編集

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,23 @@ export default function HomePage() {
 	return(
 		<div className="big-white">
 			<section className="text-gray-600 body-font	">
-				<div className="container mx-auto flex px-5 py24 items-center justify-center flex-col">
-
-					<Image
-						className="md:w-5/6 w-full mb-10 object-cover object-center rounded-2xl shadow-xl border border-gray-100"
-						alt="sorevi blog hero"
-						src="/hero.png"
-						width={1200}
-						height={500}
-						priority
-					/>
-
+				<div className="container mx-auto flex px-5 pt-0 items-center justify-center flex-col">
+					<div className="relative w-full mb-10">
+						<Image
+							className="w-full mb-10 object-cover object-center border border-gray-100"
+							alt="sorevi blog hero"
+							src="/TopImage.png"
+							width={1200}
+							height={500}
+							priority
+						/>
+						<p className="absolute inset-0 flex items-center justify-center text-white text-9xl font-bold">
+							Ploigi
+						</p>
+					</div>
 
 					<h1 className="title-font sm:text-5xl text-4xl mb-4 font-bold text-gray-900 leading-tight">
-						公開エリアトップページ<br className="hidden lg:inline-block" />
+						
 						Ploigi project
 					</h1>
 					<p className="mb-8 leading-relaxed text-lg text-gray-500 max-w-2xl mx-auto">
@@ -26,8 +29,7 @@ export default function HomePage() {
 						ここではPloigi projectの開発とその開発者の活動について記したSoreviブログを公開しています。
 					</p>
 					<div className="flex justify-center gap-4">
-						<Link href="/blog" className="inline-flex text-white bg-indigo-500 border-0 py-3 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">ブログへ</Link>
-						<Link href="/about" className="inline-flex text-gray-700 bg-gray-100 border-0 py-3 px-8 focus:outline-none hover:bg-gray-200 rounded text-lg">Ploigi projectについて</Link>
+
 					</div>
 				</div>
 			</section>
